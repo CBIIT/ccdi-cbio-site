c8275c0e448a10e822bf4b78f6a7964cd2b5ea53
johnyesit <ziao.ye@nih.gov>
2025-01-06 17:11:53 -0500

    Update 404 page

--- src/pages/status/Page404.tsx
+++ src/pages/status/Page404.tsx
@@ -1,7 +1,24 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 const Page404: FC = () => {
-  return <div>This is 404 page</div>;
+  return (
+    <div
+      style={{
+        margin: '20vh auto',
+        display: 'flex',
+        flexFlow: 'column',
+        fontWeight: 'bold',
+        fontSize: '1.5rem',
+        textAlign: 'center'
+      }}
+    >
+      Sorry, the page is Not Found
+      <Link id="return-to-home" to="/" style={{fontSize: '1rem', margin: '1rem 0'}}>
+        Return to the Home Page
+      </Link>
+    </div>
+  );
 };
 
 export default Page404;

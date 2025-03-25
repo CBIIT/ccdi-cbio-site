0098fd3adb01921ddf8521bceb7f08121aad416f
johnyesit <ziao.ye@nih.gov>
2025-03-25 16:26:06 -0400

    Adjust heights for pages

--- src/pages/iframe/IframePage.tsx
+++ src/pages/iframe/IframePage.tsx
@@ -1,24 +1,23 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 // import { useNavigate } from "react-router-dom";
 
 const IframePage: FC<{ urlParams: string, id: string, title: string }> = ({urlParams, id, title}) => {
   // const navigate = useNavigate();
-  // // CCDI-TODO: Make the iframe height fit for the content
-  // useEffect(() => {
-  //   window.addEventListener('message', (e) => {
-  //     const iframeTag = document.getElementById(id);
-  //     const eventName = e.data[0];
-  //     const data = e.data[1];
-  //     switch (eventName) {
-  //       case 'setHeight':
-  //         if (iframeTag) {
-  //           iframeTag.style.height = `${data}px`;
-  //           console.log('setHeight:', iframeTag.style.height)
-  //         }
-  //         break;
-  //     }
-  //   }, false);
-  // }, []);
+  // CCDI-TODO: Make the iframe height fit for the content
+  useEffect(() => {
+    window.addEventListener('message', (e) => {
+      const iframeTag = document.getElementById(id);
+      const eventName = e.data[0];
+      const data = e.data[1];
+      switch (eventName) {
+        case 'setHeight':
+          if (iframeTag) {
+            iframeTag.style.height = `${data}px`;
+          }
+          break;
+      }
+    }, false);
+  }, []);
   // useEffect(() => {
   //   window.addEventListener('message', (e) => {
   //     if (e.origin === process.env.REACT_APP_CCDI_CBIO_IFRAME_URL) {
@@ -32,7 +31,7 @@ const IframePage: FC<{ urlParams: string, id: string, title: string }> = ({urlPa
       src={`${process.env.REACT_APP_CCDI_CBIO_IFRAME_URL}${urlParams}`}
       id={id}
       title={title}
-      height="2400vh"
+      height="600vh"
       width="100%"
       style={{border: 'none'}}
     />
